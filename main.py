e12ceb64ed27ec05d817eaaa2b23ae45028c3e48
FunnyLamma <65251658+FunnyLamma@users.noreply.github.com>
2021-02-26 13:13:48 -0800

    updated python pg2

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ def start():
     print("Welecome")
     print("It is currently", current_time)
     catagor_laung = input("What catagory do you want?")
+    print("There is,")
+    print("Python (py)")
+    print("Javascript (js)")
+    print("Ruby (rb)")
+    print("Typescript (ts)")
     if catagor_laung == "py":
         print("Welecome to the python defenition directory!")
-        num_check1 = input("Which definition do you want?")
         print("Page 1")
         print("1. If Loops")
         print("2. While Loops")
@@ -23,6 +27,7 @@ def start():
         print("8. The JSON module")
         print("9. Modules")
         print("10. Next Page")
+        num_check1 = input("Which definition do you want?")
         if num_check1 == 1:
             with open('data/python/ifloopdef.text') as example_file:
                 example_text = example_file.read()
@@ -61,14 +66,23 @@ def start():
                 print(example_text)
         if num_check1 == 10:
             def pypage2():
-                num_check4 = input("Which definition do you want?")
                 print("Page 2")
+                print("1. Format")
                 print("10. Next Page")
+                num_check4 = input("Which definition do you want?")
+                if num_check4 == 1:
+                    with open('data/python/pyformat.txt') as example_file:
+                        example_text = example_file.read()
+                        print(example_text)
             pypage2()
+            
+                
     if catagor_laung == "js":
         print("Welecome to the javascript defenition directory!")
     if catagor_laung == "rb":
         print("Welecome to the ruby defenitiom directory!")
+        print("Page 1")
+        print("1. Format")
         num_check2 = input("What defenition do you want?")
         if num_check2 == 1:
             with open('data/ruby/rubyformatdef.text') as example_file:
@@ -76,12 +90,13 @@ def start():
                 print(example_text)
     if catagor_laung == "ts":
         print("Welecome to the typecript defenition directory!")
-    if catagor_laung == "other":
+    if catagor_laung == "ot":
         print("Welecome to the other defenition directory!")
         num_check3 = input("What defenition do you want?")
         if num_check3 == 1:
             with open('data/other/rubyvspython.text') as example_file:
                 example_text = example_file.read()
                 print(example_text)
-
+    if catagor_laung == "qu":
+        print("Welecome to the questions directory.")
 start()
