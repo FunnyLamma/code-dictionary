d0c0bb7c46ce0d17b200eebe615b4d3386bc72c3
FunnyLamma <65251658+FunnyLamma@users.noreply.github.com>
2021-03-01 13:13:53 -0800

    reformatted

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ def start():
     current_time = datetime.now()
     print("Welecome")
     print("It is currently", current_time)
-    catagor_laung = input("What catagory do you want?")
+    time.sleep(2)
     print("There is,")
     print("Python (py)")
     print("Javascript (js)")
     print("Ruby (rb)")
     print("Typescript (ts)")
     print("Questions (qu)")
+    catagor_laung = input("What catagory do you want?")
     if catagor_laung == "py":
         print("Welecome to the python defenition directory!")
         print("Page 1")
@@ -33,63 +34,76 @@ def start():
             with open('data/python/ifloopdef.text') as example_file:
                 example_text = example_file.read()
                 print(example_text)
+                time.sleep(60)
         if num_check1 == 2:
             with open('data/python/whileloopdef.txt') as example_file:
                 example_text = example_file.read()
                 print(example_text)
+                time.sleep(60)
         if num_check1 == 3:
             with open('data/python/classdef.txt') as example_file:
                 example_text = example_file.read()
                 print(example_text)
+                time.sleep(60)
         if num_check1 == 4:
             with open('data/python/booleandef.txt') as example_file:
                 example_text = example_file.read()
                 print(example_text)
+                time.sleep(60)
         if num_check1 == 5:
             with open('data/python/stringdef.txt') as example_file:
                 example_text = example_file.read()
                 print(example_text)
+                time.sleep(60)
         if num_check1 == 6:
             with open('data/python/timemoddef.txt') as example_file:
                 example_text = example_file.read()
                 print(example_text)
+                time.sleep(60)
         if num_check1 == 7:
             with open('data/python/osmoddef.txt') as example_file:
                 example_text = example_file.read()
                 print(example_text)
+                time.sleep(60)
         if num_check1 == 8:
             with open('data/python/jsonmoddef.txt') as example_file:
                 example_text = example_file.read()
                 print(example_text)
+                time.sleep(60)
         if num_check1 == 9:
             with open('data/python/moduledef.txt') as example_file:
                 example_text = example_file.read()
                 print(example_text)
+                time.sleep(60)
         if num_check1 == 10:
-            def pypage2():
-                print("Page 2")
-                print("1. Format")
-                print("2. With Statement")
-                print("3. The Datetime Module")
-                print("10. Next Page")
-                num_check4 = input("Which definition do you want?")
-                if num_check4 == 1:
-                    with open('data/python/pyformat.txt') as example_file:
-                        example_text = example_file.read()
-                        print(example_text)
-                if num_check4 == 2:
-                    with open('data/python/withstate.txt') as example_file:
-                        example_text = example_file.read()
-                        print(example_text)
-                if num_check4 == 3:
-                    with open('data/python/datetimemod.txt') as example_file:
-                        example_text = example_file.read()
-                        print(example_text)                  
-                if num_check4 == 4:
-                    with open('data/python/discordbot.txt') as example_file:
-                        example_text = example_file.read()
-                        print(example_text)  
-            pypage2()
+            time.sleep(4)
+            print("Page 2")
+            print("1. Format")
+            print("2. With Statement")
+            print("3. The Datetime Module")
+            print("10. Next Page")
+            num_check4 = input("Which definition do you want?")
+            if num_check4 == 1:
+                with open('data/python/pyformat.txt') as example_file:
+                    example_text = example_file.read()
+                    print(example_text)
+                    time.sleep(60)
+            if num_check4 == 2:
+                with open('data/python/withstate.txt') as example_file:
+                    example_text = example_file.read()
+                    print(example_text)
+                    time.sleep(60)
+            if num_check4 == 3:
+                with open('data/python/datetimemod.txt') as example_file:
+                    example_text = example_file.read()
+                    print(example_text)  
+                    time.sleep(60)                
+            if num_check4 == 4:
+                with open('data/python/discordbot.txt') as example_file:
+                    example_text = example_file.read()
+                    print(example_text)  
+                    time.sleep(60)
+            
             
                 
     if catagor_laung == "js":
